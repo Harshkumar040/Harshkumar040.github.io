3b0bcd7acb685ffbc407756dc93d691e5d22347a
Harshkumar040 <harshkumar040304@gmail.com>
2025-06-23 16:50:10 +0530

    updated

--- JS/file9.js
+++ JS/file9.js
@@ -34,17 +34,82 @@
 // console.log(cart);  
 
 
-const products=[
-    {id: 1, name: 'product 1', price: 25},
-    {id: 2, name: "product 2", price: 30},
-    {id: 3, name: "product 3", price: 45},
+// const products=[
+//     {id: 1, name: 'product 1', price: 25},
+//     {id: 2, name: "product 2", price: 30},
+//     {id: 3, name: "product 3", price: 45},
 
-];
-let cart={};
-function addToCart(productId, quantity =1){
-    const product =products.find(p=>p.id==productId);
+// ];
+// let cart={};
+// function addToCart(productId, quantity =1){
+//     const product =products.find(p=>p.id==productId);
     
-}
+// }
+
+// const score =[2,1,7,5,3];
+// score.forEach((index)=>{
+//     console.log(index);
+// });
 
 
+// const score =[2,1,7,5,3];
+// score.forEach((a,b,c)=>{
+//     console.log(c[b]);
+// });
 
+
+// const score =[2,1,7,5,3];
+// score.map((index)=>{
+//     console.log(index);
+// });
+
+
+// const score =[2,1,7,5,3];
+// const newScore=score.map((value)=>{
+//     return value
+// });
+// console.log(newScore)
+
+
+// const score =[2,1,7,5,3];
+// const newScore=score.map((value)=>{
+//     return value+5;
+// });
+// console.log(newScore)
+
+// const score =[2,1,7,5,3];
+// const newScore=score.map((value)=>{
+//     return 5;
+// });
+// console.log(newScore)
+
+
+// const score =[2,1,7,5,3];
+// const newScore=score.map((value)=>{
+//     return value > 2;
+// });
+// console.log(newScore)
+
+// const score =[2,1,7,5,3];
+// const newScore=score.filter((value)=>{
+//     return value > 2;
+// });
+// console.log(newScore)
+
+
+// const score =[2,1,7,5,3];
+// const newScore=score.find((value)=>{
+//     return value===2;
+// });
+// console.log(newScore);
+
+// const score =[2,1,7,5,2];
+// const newScore=score.filter((value)=>{
+//     return value===2;
+// });
+// console.log(newScore);
+
+
+// const score =[2,1,7,5,2];
+// const newScore=score.reduce((sum,value)=> sum + value, 0);
+// console.log(newScore);
