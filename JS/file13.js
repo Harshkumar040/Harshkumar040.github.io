9ff0f3b0618734b4abd1fe34e6761fd9cb1031e2
Harshkumar040 <harshkumar040304@gmail.com>
2025-06-24 14:08:58 +0530

    yes

--- JS/file13.js
+++ JS/file13.js
@@ -1,1 +1,12 @@
 //spread operator 
+
+// let marks={
+//     maths: 95,
+// };
+// marks={...marks, science: 54};
+// console.log(marks)
+
+
+// let students =["Harsh ", "Ananta", "Khushboo"]
+// students=[...students, "Azam"]
+// console.log(students)
---
+++ JS/file14.js
@@ -0,0 +1,16 @@
+//error handling 
+
+// try{
+//     console.log(a)
+// }
+// catch(err){
+//     console.log("Something went wrong");
+// }
+
+
+// try{
+//     console.log(a)
+// }
+// catch(err){
+//     console.log(err)
+// }
