6dbf4add140d5f56b381f9bb059afae9c2946c3d
Harshkumar040 <harshkumar040304@gmail.com>
2025-07-07 10:17:47 +0530

    updated

---
+++ nodejs/Nodeapp/index8.js
@@ -0,0 +1,8 @@
+import bcrypt from 'bcrypt'
+
+// const pwd="pass1234"
+// const hashedpwd=await bcrypt.hash(pwd, 10)
+// console.log(hashedpwd)
+
+const check=await bcrypt.compare("pass1234","$2b$10$YokWWRilkH18v4DsNHZ8g.pBEATRA0OIlLchhahStpb4Fupb42KK")
+console.log(check)
