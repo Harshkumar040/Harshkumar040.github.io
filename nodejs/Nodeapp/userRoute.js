229fad12b8827f97d5dfc0b387801217655c8dfa
Harshkumar040 <harshkumar040304@gmail.com>
2025-07-21 14:03:42 +0530

    updated

--- nodejs/Nodeapp/userRoute.js
+++ nodejs/Nodeapp/userRoute.js
@@ -1,12 +1,18 @@
-import express from 'express'
-const Router = express.Router()
-Router.get("/show", (req, res) => {
-  res.send("Show Users API");
-});
-Router.post("/register", (req, res) => {
-  res.send("Register User API");
-});
-Router.post("/login", (req, res) => {
-  res.send("User Login API");
-});
-export default Router
+import express from "express";
+import { authenticate, authorize } from "./auth.js";
+const Router = express.Router();
+import {
+  register,
+  login,
+  profile,
+  updateUser,
+  deleteUser,
+  showUsers,
+} from "./userController.js";
+Router.post("/register", register);
+Router.post("/login", login);
+Router.get("/showusers", authenticate, authorize("admin"), showUsers);
+Router.patch("/:id", authenticate, authorize("admin"), updateUser);
+Router.delete("/:id", authenticate, authorize("admin"), deleteUser);
+Router.get("/:id/profile", authenticate, profile);
+export default Router;
